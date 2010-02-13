06d92ef542e2ae2f48541e67a02acc50336e981c
Thomas E. Dickey <dickey@invisible-island.net>
2010-02-13 23:54:33 +0000

    ncurses 5.7 - patch 20100213
    + add several screen-bce.XXX entries -TD

--- dist.mk
+++ dist.mk
@@ -25,7 +25,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.745 2010/02/06 15:00:38 tom Exp $
+# $Id: dist.mk,v 1.746 2010/02/13 16:12:58 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -37,7 +37,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 5
 NCURSES_MINOR = 7
-NCURSES_PATCH = 20100206
+NCURSES_PATCH = 20100213
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
