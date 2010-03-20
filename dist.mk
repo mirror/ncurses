155b34efd7e736423212bee09a93efc96ad627de
Thomas E. Dickey <dickey@invisible-island.net>
2010-03-20 23:57:06 +0000

    ncurses 5.7 - patch 20100320
    + rename atari and st52 terminfo entries to atari-old, st52-old, use
      newer entries from FreeMiNT by Guido Flohr (from patch/report by Alan
      Hourihane).

--- dist.mk
+++ dist.mk
@@ -25,7 +25,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.750 2010/03/13 18:24:27 tom Exp $
+# $Id: dist.mk,v 1.751 2010/03/20 17:17:10 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -37,7 +37,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 5
 NCURSES_MINOR = 7
-NCURSES_PATCH = 20100313
+NCURSES_PATCH = 20100320
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
