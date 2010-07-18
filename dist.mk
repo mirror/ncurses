4573ed8af000c7a7907a59ec750da29c46c15498
Thomas E. Dickey <dickey@invisible-island.net>
2010-07-18 00:59:25 +0000

    ncurses 5.7 - patch 20100717
    * add hard-reset for rs2 to wsvt25 to help ensure that reset ends
      the alternate character set (patch by Nicholas Marriott)
    + remove tar-copy.sh and related configure/Makefile chunks, since the
      Ada95 binding is now installed using rules in Ada95/src.

--- dist.mk
+++ dist.mk
@@ -25,7 +25,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.765 2010/07/03 16:52:23 tom Exp $
+# $Id: dist.mk,v 1.766 2010/07/17 19:39:41 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -37,7 +37,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 5
 NCURSES_MINOR = 7
-NCURSES_PATCH = 20100703
+NCURSES_PATCH = 20100717
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
