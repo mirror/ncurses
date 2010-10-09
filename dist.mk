f7340994c9dc299dd54a1b4cde949027436f1599
Thomas E. Dickey <dickey@invisible-island.net>
2010-10-09 21:07:05 +0000

    ncurses 5.7 - patch 20101009
    + correct comparison used for setting 16-colors in linux-16color
      entry (Novell #644831) -TD
    + improve linux-16color entry, using "dim" for color-8 which makes it
      gray rather than black like color-0 -TD
    + drop misc/ncu-indent and misc/jpf-indent; they are provided by an
      external package "cindent".

--- dist.mk
+++ dist.mk
@@ -25,7 +25,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.776 2010/10/02 16:26:29 tom Exp $
+# $Id: dist.mk,v 1.777 2010/10/09 16:57:52 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -37,7 +37,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 5
 NCURSES_MINOR = 7
-NCURSES_PATCH = 20101002
+NCURSES_PATCH = 20101009
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
