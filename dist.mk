fc108369da39ac4082802007d06c52de80392b94
Thomas E. Dickey <dickey@invisible-island.net>
2011-09-04 00:30:36 +0000

    ncurses 5.9 - patch 20110903
    + propagate error-returns from wresize, i.e., the internal
      increase_size and decrease_size functions through resize_term (report
      by Tim van der Molen, cf:  20020713).
    + fix typo in tset manpage (patch by Sven Joachim).

--- dist.mk
+++ dist.mk
@@ -25,7 +25,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.831 2011/08/20 15:29:02 tom Exp $
+# $Id: dist.mk,v 1.832 2011/09/03 13:53:10 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -37,7 +37,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 5
 NCURSES_MINOR = 9
-NCURSES_PATCH = 20110820
+NCURSES_PATCH = 20110903
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
