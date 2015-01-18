5865ab4cec503465a610827c43b4b11b36fcda79
Thomas E. Dickey <dickey@invisible-island.net>
2015-01-18 01:43:30 +0000

    ncurses 5.9 - patch 20150117
    + improve description in INSTALL of the --with-versioned-syms option.
    + add combination of --with-hashed-db and --with-ticlib to
      configurations for ".map" files (report by Werner Fink).

--- dist.mk
+++ dist.mk
@@ -25,7 +25,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.1025 2015/01/10 15:40:16 tom Exp $
+# $Id: dist.mk,v 1.1027 2015/01/17 14:20:08 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -37,7 +37,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 5
 NCURSES_MINOR = 9
-NCURSES_PATCH = 20150110
+NCURSES_PATCH = 20150117
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
