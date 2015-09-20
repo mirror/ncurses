b9a2bd87a73d976d1c916815e2d370bf1090d1b2
Thomas E. Dickey <dickey@invisible-island.net>
2015-09-20 01:24:12 +0000

    ncurses 6.0 - patch 20150919
    + clarify in resizeterm.3x how KEY_RESIZE is pushed onto the input
      stream.
    + clarify in curs_getch.3x that the keypad mode affects ability to
      read KEY_MOUSE codes, but does not affect KEY_RESIZE.
    + add overlooked build-fix needed with Cygwin for separate Ada95
      configure script, cf: 20150606 (report by Nicolas Boulenguez)

--- dist.mk
+++ dist.mk
@@ -25,7 +25,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.1070 2015/09/12 16:17:30 tom Exp $
+# $Id: dist.mk,v 1.1071 2015/09/19 19:40:14 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -37,7 +37,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 6
 NCURSES_MINOR = 0
-NCURSES_PATCH = 20150912
+NCURSES_PATCH = 20150919
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
--- package/mingw-ncurses.spec
+++ package/mingw-ncurses.spec
@@ -3,7 +3,7 @@
 Summary: shared libraries for terminal handling
 Name: mingw32-ncurses6
 Version: 6.0
-Release: 20150912
+Release: 20150919
 License: X11
 Group: Development/Libraries
 Source: ncurses-%{version}-%{release}.tgz
