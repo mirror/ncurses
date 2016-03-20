c72b2c2c48ade76b5b4090b0f54b619a4fd483d9
Thomas E. Dickey <dickey@invisible-island.net>
2016-03-20 01:23:38 +0000

    ncurses 6.0 - patch 20160319
    + improve description of tgoto parameters (report by Steffen Nurpmeso).
    + amend workaround for Solaris line-drawing to restore a special case
      that maps Unicode line-drawing characters into the acsc string for
      non-Unicode locales (Debian #816888).

--- ncurses/widechar/lib_wacs.c
+++ ncurses/widechar/lib_wacs.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright (c) 2002-2014,2015 Free Software Foundation, Inc.              *
+ * Copyright (c) 2002-2015,2016 Free Software Foundation, Inc.              *
  *                                                                          *
  * Permission is hereby granted, free of charge, to any person obtaining a  *
  * copy of this software and associated documentation files (the            *
@@ -32,7 +32,7 @@
 
 #include <curses.priv.h>
 
-MODULE_ID("$Id: lib_wacs.c,v 1.16 2015/12/19 23:29:03 tom Exp $")
+MODULE_ID("$Id: lib_wacs.c,v 1.17 2016/03/19 19:00:09 tom Exp $")
 
 NCURSES_EXPORT_VAR(cchar_t) * _nc_wacs = 0;
 
@@ -129,9 +129,13 @@ _nc_init_wacs(void)
 #endif
 
 	    m = table[n].map;
-	    SetChar(_nc_wacs[m],
-		    table[n].value[(active && (wide == 1)) ? 1 : 0],
-		    A_NORMAL);
+	    if (active && (wide == 1)) {
+		SetChar(_nc_wacs[m], table[n].value[1], A_NORMAL);
+	    } else if (acs_map[m] & A_ALTCHARSET) {
+		SetChar(_nc_wacs[m], m, A_ALTCHARSET);
+	    } else {
+		SetChar(_nc_wacs[m], table[n].value[0], A_NORMAL);
+	    }
 
 	    T(("#%d, wide:%d SetChar(%c, %#04x) = %s",
 	       n, wide, m,
