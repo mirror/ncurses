302a066a01e4de40f08b397e87ca0e97f20870a7
Thomas E. Dickey <dickey@invisible-island.net>
2017-01-29 01:44:57 +0000

    ncurses 6.0 - patch 20170128
    + minor comment-fixes to help automate links to bug-urls -TD
    + add dvtm, dvtm-256color -TD
    + add settings corresponding to xterm-keys option to tmux entry to
      reflect upcoming change to make that option "on" by default
      (patch by Nicholas Marriott).
    + uncancel Ms in tmux entry (Harry Gindi, Nicholas Marriott).
    + add dumb-emacs-ansi -TD
    + improve discussion of early history of tput program.

--- dist.mk
+++ dist.mk
@@ -25,7 +25,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.1143 2017/01/21 13:48:23 tom Exp $
+# $Id: dist.mk,v 1.1144 2017/01/28 14:18:58 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -37,7 +37,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 6
 NCURSES_MINOR = 0
-NCURSES_PATCH = 20170121
+NCURSES_PATCH = 20170128
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
