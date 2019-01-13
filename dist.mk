acb4184f8f69fddd052a3daa8c8675f4bf8ce369
Thomas E. Dickey <dickey@invisible-island.net>
2019-01-13 01:08:21 +0000

    ncurses 6.1 - patch 20190112
    + fix typo in term(5), improve explanation of format (report by Otto
      Modinos).
    + add nsterm-direct -TD
    + use SGR 1006 mouse for konsole-base -TD
    + use SGR 1006 mouse for putty -TD
    + add ti703/ti707, ti703-w/ti707-w (Robert Clausecker)

--- dist.mk
+++ dist.mk
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 1998-2017,2018 Free Software Foundation, Inc.                #
+# Copyright (c) 1998-2018,2019 Free Software Foundation, Inc.                #
 #                                                                            #
 # Permission is hereby granted, free of charge, to any person obtaining a    #
 # copy of this software and associated documentation files (the "Software"), #
@@ -25,7 +25,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.1259 2018/12/31 10:30:35 tom Exp $
+# $Id: dist.mk,v 1.1260 2019/01/12 18:35:16 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -37,7 +37,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 6
 NCURSES_MINOR = 1
-NCURSES_PATCH = 20190105
+NCURSES_PATCH = 20190112
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
