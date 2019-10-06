8d8a3537cd58af7879c6e1921235daeed2b74926
Thomas E. Dickey <dickey@invisible-island.net>
2019-10-06 00:58:27 +0000

    ncurses 6.1 - patch 20191005
    + modify the ncurse*-config and pc-files to more closely match for the
      -I and -l options.

--- dist.mk
+++ dist.mk
@@ -25,7 +25,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.1307 2019/09/27 18:43:59 tom Exp $
+# $Id: dist.mk,v 1.1308 2019/10/05 14:44:23 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -37,7 +37,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 6
 NCURSES_MINOR = 1
-NCURSES_PATCH = 20190928
+NCURSES_PATCH = 20191005
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
