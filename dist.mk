d97989d1e0db7282c723cabb44b991b951790006
Thomas E. Dickey <dickey@invisible-island.net>
2019-12-01 01:27:53 +0000

    ncurses 6.1 - patch 20191130
    + add portability section to curs_getcchar manpage (prompted by
      discussion with Nick Black).
    + improve portability discussion of ACS characters in curs_addch
      manpage.
    + improve typography for double-quotes in manpages.

--- dist.mk
+++ dist.mk
@@ -25,7 +25,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.1316 2019/11/23 15:45:17 tom Exp $
+# $Id: dist.mk,v 1.1317 2019/11/30 14:52:31 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -37,7 +37,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 6
 NCURSES_MINOR = 1
-NCURSES_PATCH = 20191123
+NCURSES_PATCH = 20191130
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
