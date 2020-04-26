89730563d0a660d4ddd83d28660dc23c6d3f0bed
Thomas E. Dickey <dickey@invisible-island.net>
2020-04-26 01:15:23 +0000

    ncurses 6.2 - patch 20200425
    + use vt100+fnkeys in putty -TD
    + fix a typo in tput.1; "columns" should be "cols".

--- dist.mk
+++ dist.mk
@@ -26,7 +26,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.1346 2020/04/18 09:00:18 tom Exp $
+# $Id: dist.mk,v 1.1347 2020/04/25 09:36:39 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -38,7 +38,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 6
 NCURSES_MINOR = 2
-NCURSES_PATCH = 20200418
+NCURSES_PATCH = 20200425
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
--- package/mingw-ncurses.nsi
+++ package/mingw-ncurses.nsi
@@ -1,4 +1,4 @@
-; $Id: mingw-ncurses.nsi,v 1.390 2020/04/18 09:00:18 tom Exp $
+; $Id: mingw-ncurses.nsi,v 1.391 2020/04/25 09:36:39 tom Exp $
 
 ; TODO add examples
 ; TODO bump ABI to 6
@@ -10,7 +10,7 @@
 !define VERSION_MAJOR "6"
 !define VERSION_MINOR "2"
 !define VERSION_YYYY  "2020"
-!define VERSION_MMDD  "0418"
+!define VERSION_MMDD  "0425"
 !define VERSION_PATCH ${VERSION_YYYY}${VERSION_MMDD}
 
 !define MY_ABI   "5"
