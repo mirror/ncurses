e5d1530ca229aef94a3c84ad33f8ae89f35c4045
Thomas E. Dickey <dickey@invisible-island.net>
2020-10-18 01:25:54 +0000

    ncurses 6.2 - patch 20201017
    + improve manpage typography.
    + improve discussion in curs_addch.3x of the use of unctrl to display
      nonprintable characters.
    + add a note in terminfo.5 explaining that no-parameter strings such
      as sgr0 or cnorm should not be used with tparm.

--- dist.mk
+++ dist.mk
@@ -26,7 +26,7 @@
 # use or other dealings in this Software without prior written               #
 # authorization.                                                             #
 ##############################################################################
-# $Id: dist.mk,v 1.1380 2020/10/10 08:40:40 tom Exp $
+# $Id: dist.mk,v 1.1381 2020/10/17 11:56:23 tom Exp $
 # Makefile for creating ncurses distributions.
 #
 # This only needs to be used directly as a makefile by developers, but
@@ -38,7 +38,7 @@ SHELL = /bin/sh
 # These define the major/minor/patch versions of ncurses.
 NCURSES_MAJOR = 6
 NCURSES_MINOR = 2
-NCURSES_PATCH = 20201010
+NCURSES_PATCH = 20201017
 
 # We don't append the patch to the version, since this only applies to releases
 VERSION = $(NCURSES_MAJOR).$(NCURSES_MINOR)
